e1e514b1aa2d1f59f6716098bec0c66c7d95a4ea
kekg602 <kekg602@yahoo.com>
2016-03-01 12:21:54 -0500

    Adding routes

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -5,8 +5,24 @@
 
     function Configure($routeProvider){
         $routeProvider
+            .when("/register",
+             {
+                templateUrl: "/views/users/register.view.html"
+             })
+            .when("/login",
+            {
+               templateUrl: "/views/users/login.view.html"
+            })
+            .when("/profile",
+            {
+                templateUrl: "/views/users/profile.view.html"
+            })
+            .when("/admin",
+            {
+                templateUrl: "/views/users/admin.view.html"
+            })
             .otherwise({
-                redirectTo: "/home/home.view.html"
+                redirectTo: "/views/home/home.view.html"
             });
     }
 })();
