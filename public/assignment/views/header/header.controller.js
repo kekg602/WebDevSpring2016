25f130d62d22205ffe44f61cd7b8897933c896d7
kekg602 <kekg602@yahoo.com>
2016-03-01 12:47:12 -0500

    Adding correct options to header

---
+++ public/assignment/views/header/header.controller.js
@@ -0,0 +1,17 @@
+(function()
+{
+    angular
+        .module("FormBuilderApp", ["ngRoute"])
+        .controller("HeaderController", HeaderController);
+
+    function HeaderController($scope, $location, UserService)
+    {
+        $scope.$location = $location;
+        $scope.logout = logout;
+
+        function logout() {
+            UserService.setCurrentUser(null);
+            $location.url("/home");
+        }
+    }
+})();
