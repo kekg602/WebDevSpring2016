ddadf85b61649efabd83358d0ff735399654d25d
kekg602 <kekg602@yahoo.com>
2016-03-03 14:29:23 -0500

    Adding form add functionality

--- public/assignment/views/forms/forms.controller.js
+++ public/assignment/views/forms/forms.controller.js
@@ -6,8 +6,17 @@
         .controller("FormController", FormController);
 
     function FormController(FormService, $scope, $rootScope){
+
+        // get current user
         $scope.user = $rootScope.currentUser;
 
+        // set up event handlers
+        $scope.addForm = addForm;
+        //$scope.updateForm = updateForm;
+        //$scope.deleteForm = deleteForm;
+        //$scope.selectForm = selectForm;
+
+        // get forms for current user
         if ($scope.user){
             FormService.findAllFormsForUser($scope.user._id, function(forms){
                 if (forms){
@@ -15,6 +24,17 @@
                 }
             });
         }
+
+        // add a form
+        function addForm(form){
+            FormService.createFormForUser($scope.user._id, form, function(newForm){
+               if (newForm){
+                   $scope.forms.push(newForm);
+               }
+            });
+        }
+
+
 
 
 
