123714121c15e5ee7ddc7ce5ec425d913e458dab
kekg602 <kekg602@yahoo.com>
2016-03-17 23:28:04 -0400

    Updating form client service

--- public/assignment/client/services/forms.service.client.js
+++ public/assignment/client/services/forms.service.client.js
@@ -7,11 +7,6 @@
 
     function FormService(){
         var model = {
-            forms: [
-                {"_id": "000", "title": "Contacts", "userId": 123},
-                {"_id": "010", "title": "ToDo", "userId": 123},
-                {"_id": "020", "title": "CDs", "userId": 234},
-            ],
             createFormForUser: createFormForUser,
             findAllFormsForUser: findAllFormsForUser,
             deleteFormById: deleteFormById,
@@ -19,45 +14,20 @@
         };
         return model;
 
-        function createFormForUser(userId, form, callback){
-            var form = {
-                _id: (new Date).getTime(),
-                title: form.title,
-                userId: userId
-            };
-            model.forms.push(form);
-            callback(form);
+        function createFormForUser(userId, form){
+            return $http.post ("/api/assignment/user/" + userId + "/form", form);
         }
 
-        function findAllFormsForUser(userId, callback){
-            var userForms = [];
-            for (var u in model.forms){
-                if (model.forms[u].userId === userId){
-                    userForms.push(model.forms[u]);
-                }
-            }
-            callback(userForms);
+        function findAllFormsForUser(userId){
+            return $http.get ("/api/assignment/user/" + userId + "/form");
         }
 
-        function deleteFormById(formId, callback){
-            for (var u in model.forms){
-                if (model.forms[u]._id === formId){
-                    model.forms.splice(u, 1);
-                }
-            }
-            callback(model.forms);
+        function deleteFormById(formId){
+            return $http.delete ("/api/assignment/form/" + formId);
         }
 
-        function updateFormById(formId, newForm, callback){
-            for (var u in model.forms){
-                if (model.forms[u]._id === formId){
-                    model.forms[u].title = newForm.title;
-                    model.forms[u].userId = newForm.userId;
-                    callback(model.forms[u]);
-                    return;
-                }
-            }
-            callback(null);
+        function updateFormById(formId, newForm){
+            return $http.put ("/api/assignment/form/" + formId, newForm);
         }
     }
 })();
