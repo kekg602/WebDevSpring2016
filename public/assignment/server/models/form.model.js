650eea6011507e40fe9e2157ccd937d4aa5148b2
Katie Gilligan <Katie Gilligan>
2016-03-17 22:10:30 -0400

    Adding form service

--- public/assignment/server/models/form.model.js
+++ public/assignment/server/models/form.model.js
@@ -10,7 +10,10 @@ module.exports = function() {
         findFormByTitle: findFormByTitle,
         findFormsByUserId: findFormsByUserId,
         updateForm: updateForm,
-        deleteForm: deleteForm
+        deleteForm: deleteForm,
+        findFieldByFormId: findFieldByFormId,
+        deleteField: deleteField,
+        updateField: updateField
     };
     return api;
 
@@ -81,6 +84,72 @@ module.exports = function() {
         for (var f in mock){
             if (mock[f]._id === formId){
                 mock.splice(f, 1);
+            }
+        }
+        return mock;
+    }
+
+    // return the fields in a given form
+    function findFieldByFormId(formId){
+        for (var f in mock){
+            if (mock[f]._id === formId){
+                return mock[f].fields;
+            }
+        }
+        return null;
+    }
+
+    // return a field from a specific form, with a specific id
+    function findFieldByFormIdAndFieldId(formId, fieldId){
+        for (var f in mock){
+            if (mock[f]._id === formId){
+                for (var fi in mock[f].fields){
+                    if (mock[f].fields[fi]._id === fieldId){
+                        return mock[f].fields[fi];
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    // delete a specific field
+    function deleteField(formId, fieldId){
+        for (var f in mock){
+            if (mock[f]._id === formId){
+                for (var fi in mock[f].fields){
+                    if (mock[f].fields[fi]._id === fieldId){
+                        mock[f].fields.splice(fi, 1);
+                    }
+                }
+            }
+        }
+        return mock;
+    }
+
+    // create a field in a form
+    function createField(formId, field){
+        field._id = uuid.v1();
+        for (var f in mock){
+            if (mock[f]._id === formId){
+                mock[f].fields.push(field);
+            }
+        }
+        return mock;
+    }
+
+    // update a field in a form
+    function updateField(formId, field, fieldId){
+        for (var f in mock){
+            if (mock[f]._id === formId){
+                for (var fi in mock[f].fields){
+                    if (mock[f].fields[fi]._id === fieldId){
+                        mock[f].fields[fi].label = field.label;
+                        mock[f].fields[fi].type = field.type;
+                        mock[f].fields[fi].placeholder = field.placeholder;
+                        mock[f].fields[fi].options = field.options;
+                    }
+                }
             }
         }
         return mock;
