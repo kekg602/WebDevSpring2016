1845ab8ae8564303097eabe25fad0bb38e3cfdd3
kekg602 <kekg602@yahoo.com>
2016-03-23 21:48:58 -0400

    Adding user schedule availability client service

---
+++ public/project/client/services/userScheduleAvailability.service.client.js
@@ -0,0 +1,34 @@
+(function(){
+    'use strict';
+
+    angular
+        .module("TennisSchedulerApp")
+        .factory("UserScheduleAvailabilityService", UserScheduleAvailabilityService);
+
+    function UserScheduleAvailabilityService(){
+        var model = {
+            createAvailabilityEntry: createAvailabilityEntry,
+            findAvailabilityEntry: findAvailabilityEntry,
+            updateAvailabilityEntry: updateAvailabilityEntry,
+            deleteAvailabilityEntry: deleteAvailabilityEntry
+        };
+        return model;
+
+        function createAvailabilityEntry(entry){
+            return $http.post("/api/project/avail", entry);
+        }
+
+        function findAvailabilityEntry(userId, scheduleId){
+            return $http.get("/api/project/avail?userId=" + userId + "&scheduleId" + scheduleId);
+        }
+
+        function deleteAvailabilityEntry(userId, scheduleId){
+            return $http.delete("/api/project/avail/" + userId + "/" + scheduleId);
+        }
+
+        function updateAvailabilityEntry(userId, scheduleId, updatedAvailability){
+            return $http.put("/api/project/avail/" + userId + "/" + scheduleId, updatedAvailability);
+        }
+
+    }
+})();
--- public/project/server/services/userScheduleAvailability.service.server.js
+++ public/project/server/services/userScheduleAvailability.service.server.js
@@ -1,9 +1,9 @@
 module.exports = function(app, userScheduleAvailabilityModel){
 
-    app.post("/api/project/schedule", createAvailabilityEntry);
-    app.get("/api/project/schedule", findAvailabilityEntry);
-    app.put("/api/project/schedule", updateAvailabilityEntry);
-    app.delete("/api/project/schedule", deleteAvailabilityEntry);
+    app.post("/api/project/avail", createAvailabilityEntry);
+    app.get("/api/project/avail", findAvailabilityEntry);
+    app.put("/api/project/avail/:userId/:scheduleId", updateAvailabilityEntry);
+    app.delete("/api/project/avail/:userId/:scheduleId", deleteAvailabilityEntry);
 
     function createAvailabilityEntry(req, res){
         var entry = req.body;
@@ -19,15 +19,16 @@ module.exports = function(app, userScheduleAvailabilityModel){
     }
 
     function updateAvailabilityEntry(req, res){
-        var userId = req.query.userId;
-        var scheduleId = req.query.scheduleId;
-        var entries = userScheduleAvailabilityModel.updateAvailabilityEntry(userId, scheduleId);
+        var userId = req.params.userId;
+        var scheduleId = req.params.scheduleId;
+        var avail = req.body;
+        var entries = userScheduleAvailabilityModel.updateAvailabilityEntry(userId, scheduleId, avail);
         res.json(entries);
     }
 
     function deleteAvailabilityEntry(req, res){
-        var userId = req.query.userId;
-        var scheduleId = req.query.scheduleId;
+        var userId = req.params.userId;
+        var scheduleId = req.params.scheduleId;
         var entries = userScheduleAvailabilityModel.deleteAvailabilityEntry(userId, scheduleId);
         res.json(entries);
     }
