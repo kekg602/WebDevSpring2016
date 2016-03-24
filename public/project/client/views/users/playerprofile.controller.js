4e9ff49d03f5a6b3d12dedb93b303a66978866e3
kekg602 <kekg602@yahoo.com>
2016-03-24 13:31:57 -0400

    Show who a user plays with on their user profile

--- public/project/client/views/users/playerprofile.controller.js
+++ public/project/client/views/users/playerprofile.controller.js
@@ -5,11 +5,12 @@
         .module("TennisSchedulerApp")
         .controller("PlayerProfileController", PlayerProfileController);
 
-    function PlayerProfileController($scope, UserService, $routeParams) {
+    function PlayerProfileController($scope, $rootScope, UserService, ScheduleService, $routeParams) {
 
         $scope.playerId = $routeParams.playerId;
+        //$scope.goToProfile = goToProfile;
 
-        // get this players information
+        // get this players information and schedule
         if ($scope.playerId){
             UserService
                 .findUserById($scope.playerId)
@@ -18,11 +19,52 @@
 
         // set the user so we can show his/her profile data
         function userFoundResponse(player){
-            console.log(player);
-
             if (player.data){
                 $scope.user = player.data;
+
+                getUserSchedules();
             }
         }
+
+        // get user schedules in order to render list of who they play with
+        function getUserSchedules(){
+            if ($scope.user.username){
+                console.log($scope.user.username);
+
+                ScheduleService
+                    .findSchedulesByUsername($scope.user.username)
+                    .then(renderPlaysWithDetails);
+            }
+        }
+
+        // show who the user plays tennis with on their profile page
+        function renderPlaysWithDetails(schedules){
+            if (schedules.data){
+
+                $scope.schedules = schedules.data;
+
+                var playsWith = [];
+                for (var s in schedules.data){
+                    for (var p in schedules.data[s].players){
+
+                        console.log(playsWith.indexOf(schedules.data[s].players[p]));
+
+                        // if the user is not in the list yet, add them
+                        if (playsWith.indexOf(schedules.data[s].players[p]) <= -1 &&
+                        schedules.data[s].players[p] != $scope.user.username){
+                            playsWith.push(schedules.data[s].players[p]);
+                        }
+                    }
+                }
+                $scope.playsWith = playsWith;
+            }
+        }
+
+        /*
+        function profilePage(index){
+            $scope.goToPlayerUsername = $scope.playsWith[index];
+            $location.path('/profile/' +  $scope.goToPlayerId);
+            console.log($scope.playerId);
+        }*/
     }
 })();
