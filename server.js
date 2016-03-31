eb38eb8dc7a35ed8830e83aa9918411ff1735f63
kekg602 <kekg602@yahoo.com>
2016-03-31 10:42:04 -0400

    Adding check for remote database in server.js

--- server.js
+++ server.js
@@ -2,9 +2,21 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/test');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+
+var connectionString = 'mongodb://127.0.0.1:27017/test'
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+// connect to the database
+var db = mongoose.connect(connectionString);
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
