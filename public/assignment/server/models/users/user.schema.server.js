1b86f84060fbbe9c3c1335d0385fdd8c7e7e4b53
kekg602 <kekg602@yahoo.com>
2016-03-31 10:01:15 -0400

    Adding in field schema

--- public/assignment/server/models/users/user.schema.server.js
+++ public/assignment/server/models/users/user.schema.server.js
@@ -3,12 +3,12 @@ var mongoose = require("mongoose");
 module.exports = function () {
 
     var UserSchema = mongoose.Schema({
-        username: {type: String, description: "alice"},
-        password: {type: String, description: "p@ssw0rd"},
-        firstName: {type: String, description: "Alice"},
-        lastName: {type: String, description: "Wonderland"},
-        emails: {type: [String], description: ['alice@wonderland.com', 'alice@gmail.com']},
-        phones: {type: [String], description: ['123-234-4321', '234-432-2344']}
+        username: String,
+        password: String,
+        firstName: String,
+        lastName: String,
+        emails: [String],
+        phones: [String]
     }, {collection: "user"});
 
     return UserSchema;
