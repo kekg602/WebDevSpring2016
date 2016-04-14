fc8e670bfad47457628250ba513bb20f6969d9dd
kekg602 <kekg602@yahoo.com>
2016-04-14 15:56:24 -0400

    Admins can add a user

--- public/assignment/client/views/admin/admin.controller.js
+++ public/assignment/client/views/admin/admin.controller.js
@@ -7,14 +7,45 @@
         .controller("AdminController", AdminController);
 
     function AdminController($scope, $location, $rootScope, AdminService) {
+        $scope.errorMessage = null;
+        $scope.addUser = addUser;
+
+        findAllUsers();
+
         // get all users to populate page
-        AdminService
-            .findAllUsers()
-            .then(renderUsers);
+        function findAllUsers(){
+            AdminService
+                .findAllUsers()
+                .then(renderUsers);
+        }
 
         function renderUsers(users){
             if (users.data){
                 $scope.users = users.data;
+            }
+        }
+
+        function addUser(newUser){
+            // make sure required fields are filled out
+            if (!newUser.username){
+                $scope.errorMessage = "Please provide a username";
+                return;
+            } else if (!newUser.password){
+                $scope.errorMessage = "Please provide a password";
+                return;
+            }
+
+            AdminService
+                .createUser(newUser)
+                .then(addUserResponse);
+        }
+
+        function addUserResponse(newUser){
+            if (newUser.data){
+                $scope.user = {};
+                findAllUsers();
+            } else {
+                $scope.errorMessage = "Error adding user";
             }
         }
     }
