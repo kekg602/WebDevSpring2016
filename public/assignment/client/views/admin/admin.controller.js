ce21f1a535c5ed93a971009b111d0d686a4c1d31
kekg602 <kekg602@yahoo.com>
2016-04-14 01:47:25 -0400

    Get all users to show in the admin view

--- public/assignment/client/views/admin/admin.controller.js
+++ public/assignment/client/views/admin/admin.controller.js
@@ -7,7 +7,16 @@
         .controller("AdminController", AdminController);
 
     function AdminController($scope, $location, $rootScope, AdminService) {
+        // get all users to populate page
+        AdminService
+            .findAllUsers()
+            .then(renderUsers);
 
+        function renderUsers(users){
+            if (users.data){
+                $scope.users = users.data;
+            }
+        }
     }
 
 
