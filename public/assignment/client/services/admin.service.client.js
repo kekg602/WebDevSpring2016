25a129368ff41fd50989e89a7b063b2194a529d0
kekg602 <kekg602@yahoo.com>
2016-04-14 01:06:05 -0400

    Adding functions to admin client service

--- public/assignment/client/services/admin.service.client.js
+++ public/assignment/client/services/admin.service.client.js
@@ -7,9 +7,32 @@
 
     function AdminService($http) {
         var api = {
-
+            createUser: createUser,
+            findAllUsers: findAllUsers,
+            findUserById: findUserById,
+            deleteUser: deleteUser,
+            updateUser: updateUser
         };
         return api;
 
+        function createUser(user){
+            return $http.post ("/api/assignment/admin/user", user);
+        }
+
+        function findAllUsers(){
+            return $http.get ("/api/assignment/admin/user");
+        }
+
+        function findUserById(userId){
+            return $http.get("/api/assignment/admin/user/" + userId);
+        }
+
+        function deleteUser(userId){
+            return $http.delete("/api/assignment/admin/user" + userId);
+        }
+
+        function updateUser(userId, updatedUser){
+            return $http.put ("/api/assignment/admin/user" + userId, updatedUser);
+        }
     }
 })();
